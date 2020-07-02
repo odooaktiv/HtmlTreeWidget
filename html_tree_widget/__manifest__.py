30ddd1969619531db2be988865734d607af42f3e
odooaktiv <odoo@aktivsoftware.com>
2020-07-02 18:37:51 +0530

    [IMP] set category

--- html_tree_widget/__manifest__.py
+++ html_tree_widget/__manifest__.py
@@ -11,7 +11,7 @@
 
     'author': "Aktiv Software",
     'website': "http://www.aktivsoftware.com",
-    'category': 'Web',
+    'category': 'Extra Tools',
     'license': 'AGPL-3',
     'version': '11.0.1.0.0',
     'depends': ['base'],
